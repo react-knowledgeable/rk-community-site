64c07f47ba618ad2ec7f241c633006218713088b
Wei Gao <wgao19@gmail.com>
2019-07-13 00:18:34 +0800

    add favico

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,37 @@ module.exports = {
   pathPrefix: '/',
   plugins: [
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/static/logo.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-sass`,
       options: {
         implementation: require('sass'),
