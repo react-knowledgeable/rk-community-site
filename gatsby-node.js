28f3f1d9df4555e4bd3f119bf84272ba91e82475
Wei Gao <wgao19@gmail.com>
2019-07-15 16:24:03 +0800

    Add redirect to intro slides

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,6 +18,7 @@ exports.createPages = ({ graphql, actions }) => {
   
   createRedirect({ fromPage: '/speak', toPage: 'https://github.com/react-knowledgeable/talks', isPermanent: true, redirectInBrowser: true });
   createRedirect({ fromPage: '/meetup', toPage: 'https://meetup.com/React-Knowledgeable', isPermanent: true, redirectInBrowser: true });
+  createRedirect({ fromPage: '/hello', toPage: 'https://hello-rk-lightning.netlify.com/', isPermanent: true, redirectInBrowser: true });
 
   // const talkTemplate = path.resolve(`./src/templates/Talk/index.jsx`);
   const storyTemplate = path.resolve(`./src/templates/Story/index.jsx`);
