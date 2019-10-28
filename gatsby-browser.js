d4dd771c491fb0aa77524bde7632e4bc5b5cc67c
Wei Gao <wgao19@gmail.com>
2019-10-28 18:13:08 +0800

    use concurrent react

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+export const replaceHydrateFunction = () => {
+  return (element, container, callback) => {
+    ReactDOM.createRoot(container).render(element, callback);
+  };
+};
