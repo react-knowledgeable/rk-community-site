c97d91051c3015b3ad0229a6c586ae2397e96f3b
Wei Gao <wgao19@gmail.com>
2019-10-28 22:24:57 +0800

    do not call ReactDOM.hydrate on client side

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom';
 
 export const replaceHydrateFunction = () => {
   return (element, container, callback) => {
-    ReactDOM.createRoot(container).render(callback(element));
+    ReactDOM.createRoot(container).render(
+      typeof callback === 'function' && window === undefined // use window object to check for SSR
+        ? callback(element) // callback is by default ReactDOM.hydrate
+        : element
+    );
   };
 };
