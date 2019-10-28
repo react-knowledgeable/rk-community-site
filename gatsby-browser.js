d31c9e4ef3c0630cde6c837e91645eb841693ab4
Wei Gao <wgao19@gmail.com>
2019-10-29 00:19:42 +0800

    wip: enable concurrent react (again) (#63)
    * enable concurrent react (again)
    
    * run the callback (hydrate) function on element before rendering
    
    * do not call ReactDOM.hydrate on client side
    
    * call callback(element) on production

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+
+export const replaceHydrateFunction = () => {
+  return (element, container, callback) => {
+    ReactDOM.createRoot(container).render(
+      process.env.NODE_ENV === 'production' ? callback(element) : element
+    );
+  };
+};
