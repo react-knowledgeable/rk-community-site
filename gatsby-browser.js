79145c3b244782b0f632e900a9559b50758c4e52
Wei Gao <wgao19@gmail.com>
2019-11-03 01:14:20 +0800

    proper replace hydrate fn

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom';
 
+// https://twitter.com/EphemeralCircle/status/1190670453221842944?s=20
+// our own @thchia actually also pointed this out as well
+// https://github.com/react-knowledgeable/rk-community-site/issues/70#issuecomment-549008052
+// took me much more hassle to figure this, apologize for my silliness
 export const replaceHydrateFunction = () => {
   return (element, container, callback) => {
-    ReactDOM.createRoot(container).render(
-      ReactDOM.hydrate(element, container, callback)
-    );
+    ReactDOM.createRoot(container, {
+      hydrate: true,
+      hydrationOptions: { onHydrated: callback },
+    }).render(element);
   };
 };
