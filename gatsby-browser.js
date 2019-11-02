bb66dc20333a47a1c92d3f26b9f2b3bee266afff
Wei Gao <wgao19@gmail.com>
2019-11-02 22:12:50 +0800

    properly replace hydrate function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,7 @@ import ReactDOM from 'react-dom';
 
 export const replaceHydrateFunction = () => {
   return (element, container, callback) => {
-    ReactDOM.createRoot(container).render(
-      process.env.NODE_ENV === 'production' ? callback(element) : element
-    );
+    ReactDOM.hydrate(element, container, callback);
+    ReactDOM.createRoot(container).render(element);
   };
 };
