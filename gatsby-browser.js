547a66babc9615d7d5dcca6e3b15cc946ecfa94a
Wei Gao <wgao19@gmail.com>
2019-11-02 23:09:15 +0800

    fix duplicated content on client side rendering

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 export const replaceHydrateFunction = () => {
   return (element, container, callback) => {
     ReactDOM.hydrate(element, container, callback);
-    ReactDOM.createRoot(container).render(element);
+    ReactDOM.createRoot(container).render(
+      process.env.NODE_ENV === 'production' ? callback(element) : element
+    );
   };
 };
