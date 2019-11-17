70914387b3d6720a88a52fabe4ce51d42e47f381
Thomas Chia <thomas.h.chia@googlemail.com>
2019-11-17 16:12:06 +0800

    Fixed undefined errMessage

--- src/functions/airtable.js
+++ src/functions/airtable.js
@@ -1,78 +1,81 @@
-import Airtable from 'airtable';
+import Airtable from 'airtable'
+
+const ERROR_MSGS = {
+  UNSUPPORTED_METHOD: 'Unsupported method',
+  UNKNOWN_ERROR: 'Server Error',
+}
 
 export const handler = async (event, _, callback) => {
   try {
-    const atClient = _configureAirtable();
-    switch(event.httpMethod) {
-      case "POST":
+    const atClient = _configureAirtable()
+    switch (event.httpMethod) {
+      case 'POST':
         await insertAttendee(atClient, event, callback)
         break
-      case "GET":
+      case 'GET':
         await retrieveAttendees(atClient, event, callback)
         break
       default:
-        callback(Error({ message: errMessage }), {
+        callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
           status: 405,
-          body: errMessage,
-        });
+          body: ERROR_MSGS.UNSUPPORTED_METHOD,
+        })
     }
   } catch (e) {
     callback(Error(e), {
       status: 500,
-      body: e,
-    });
+      body: ERROR_MSGS.UNKNOWN_ERROR,
+    })
   }
-};
+}
 
 async function retrieveAttendees(Client, event, callback) {
-  let attendees = [];
+  let attendees = []
   let selectOpts = {}
-    const {eventId} = event.queryStringParameters
-    if (eventId) {
-      selectOpts = {filterByFormula: `SEARCH("${eventId}",{Event ID})`}
-    }
-    await Client('Attendees')
-      .select(selectOpts)
-      .eachPage((records, fetchNextPage) => {
-        records.forEach(function(record) {
-          attendees.push(record.fields);
-        });
-        fetchNextPage();
-      });
-    callback(null, {
-      status: 200,
-      body: JSON.stringify(attendees),
-    });
+  const { eventId } = event.queryStringParameters
+  if (eventId) {
+    selectOpts = { filterByFormula: `SEARCH("${eventId}",{Event ID})` }
+  }
+  await Client('Attendees')
+    .select(selectOpts)
+    .eachPage((records, fetchNextPage) => {
+      records.forEach(function(record) {
+        attendees.push(record.fields)
+      })
+      fetchNextPage()
+    })
+  callback(null, {
+    status: 200,
+    body: JSON.stringify(attendees),
+  })
 }
 
 async function insertAttendee(Client, event, callback) {
-  if (event.httpMethod !== "POST") {
-    const errMessage = "Unsupported method"
-    return callback(Error({ message: errMessage }), {
+  if (event.httpMethod !== 'POST') {
+    return callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
       status: 405,
-      body: errMessage,
-    });
+      body: ERROR_MSGS.UNSUPPORTED_METHOD,
+    })
   }
-  const { eventId, name, username } = JSON.parse(event.body);
-  await Client('Attendees')
-    .create([
-      {
-        fields: {
-          Name: name,
-          'Github Username': username,
-          'Event ID': eventId,
-          Type: 'Attendee',
-          'Created Date': new Date().toISOString(),
-        },
+  const { eventId, name, username } = JSON.parse(event.body)
+  await Client('Attendees').create([
+    {
+      fields: {
+        Name: name,
+        'Github Username': username,
+        'Event ID': eventId,
+        Type: 'Attendee',
+        'Created Date': new Date().toISOString(),
       },
-    ])
+    },
+  ])
   callback(null, {
     status: 200,
     body: JSON.stringify({ name, eventId }),
-  });
+  })
 }
 
 function _configureAirtable() {
-  Airtable.configure({ apiKey: '__AIRTABLE_API_KEY__' });
-  return Airtable.base('__AIRTABLE_BASE_ID__');
+  Airtable.configure({ apiKey: '__AIRTABLE_API_KEY__' })
+  return Airtable.base('__AIRTABLE_BASE_ID__')
 }
