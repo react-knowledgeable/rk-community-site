d73cdc66dccf446ce0d96d78cb9cd2e45e4b264c
Thomas Chia <thomas.h.chia@googlemail.com>
2019-11-23 19:43:32 +0800

    Fix some dodgy code

--- src/pages/LoginCallback/index.jsx
+++ src/pages/LoginCallback/index.jsx
@@ -6,7 +6,13 @@ import { navigate } from '@reach/router';
 const LoginCallback = () => {
   React.useEffect(() => {
     const { code, state } = qs.parse(window.location.search);
-    axios(`/.netlify/functions/auth?code=${code}&state=${state}`).then(res => {
+    axios({
+      method: 'get',
+      url: `/.netlify/functions/auth?code=${code}&state=${state}`,
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then(res => {
       localStorage.setItem('RK_auth_token', res.access_token);
       navigate('/');
     });
