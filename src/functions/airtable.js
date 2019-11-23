925dec4ac712e7508e6ed84586564fe7e71cf2df
Thomas Chia <thomas.h.chia@googlemail.com>
2019-11-23 22:26:59 +0800

    Refactor airtable functions. Don't insert duplicate records, and allow removal.

--- src/functions/airtable.js
+++ src/functions/airtable.js
@@ -1,4 +1,5 @@
 import Airtable from 'airtable';
+import axios from 'axios';
 
 const ERROR_MSGS = {
   UNSUPPORTED_METHOD: 'Unsupported method',
@@ -8,51 +9,43 @@ const ERROR_MSGS = {
 export const handler = async (event, _, callback) => {
   try {
     const atClient = _configureAirtable();
+    const atService = AirtableService(atClient);
     switch (event.httpMethod) {
-      case 'POST':
-        await insertAttendee(atClient, event, callback);
-        break;
       case 'GET':
-        await retrieveAttendees(atClient, event, callback);
+        await retrieveAttendees(atService, event, callback);
+        break;
+      case 'POST':
+        await insertAttendee(atService, event, callback);
+        break;
+      case 'DELETE':
+        await removeAttendee(atService, event, callback);
         break;
       default:
         callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
-          status: 405,
+          statusCode: 405,
           body: ERROR_MSGS.UNSUPPORTED_METHOD,
         });
     }
   } catch (e) {
     callback(Error(e), {
-      status: 500,
+      statusCode: 500,
       body: ERROR_MSGS.UNKNOWN_ERROR,
     });
   }
 };
 
 async function retrieveAttendees(Client, event, callback) {
-  let attendees = [];
-  let selectOpts = {};
-  let filterConfig = [];
+  let attendees;
   const { eventId, username } = event.queryStringParameters;
-  if (eventId) {
-    filterConfig.push(`SEARCH("${eventId}",{Event ID})`);
+  if (eventId && username) {
+    attendees = Client.getSingleAttendee({ eventId, username });
+  } else if (eventId) {
+    attendees = Client.listAttendees({ eventId });
+  } else {
+    throw new Error('Missing parameters');
   }
-  if (username) {
-    filterConfig.push(`SEARCH("${username}",{Github Username})`);
-  }
-  if (filterConfig.length > 0) {
-    selectOpts = `AND(${filterConfig.join(',')})`;
-  }
-  await Client('Attendees')
-    .select(selectOpts)
-    .eachPage((records, fetchNextPage) => {
-      records.forEach(function(record) {
-        attendees.push(record.fields);
-      });
-      fetchNextPage();
-    });
   callback(null, {
-    status: 200,
+    statusCode: 200,
     body: JSON.stringify(attendees),
   });
 }
@@ -60,29 +53,124 @@ async function retrieveAttendees(Client, event, callback) {
 async function insertAttendee(Client, event, callback) {
   if (event.httpMethod !== 'POST') {
     return callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
-      status: 405,
+      statusCode: 405,
       body: ERROR_MSGS.UNSUPPORTED_METHOD,
     });
   }
-  const { eventId, name, username } = JSON.parse(event.body);
-  await Client('Attendees').create([
-    {
-      fields: {
-        Name: name,
-        'Github Username': username,
-        'Event ID': eventId,
-        Type: 'Attendee',
-        'Created Date': new Date().toISOString(),
-      },
+  const userDetails = await axios({
+    method: 'GET',
+    url: 'https://api.github.com/user',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: event.headers.Authorization,
     },
-  ]);
+  });
+  const {
+    data: { login, name },
+  } = userDetails;
+  const { eventId } = JSON.parse(event.body);
+  const userExists = Client.getSingleAttendee({ eventId, username: login });
+  if (userExists) {
+    return callback(null, {
+      statusCode: 409,
+      body: 'User already exists',
+    });
+  }
+  await Client.insertAttendee({ eventId, name, login });
   callback(null, {
-    status: 200,
+    statusCode: 200,
     body: JSON.stringify({ name, eventId }),
   });
 }
 
+async function removeAttendee(Client, event, callback) {
+  const { eventId } = event.queryStringParameters;
+  if (!eventId) {
+    throw new Error('Missing Parameters: eventId');
+  }
+  const userDetails = await axios({
+    method: 'GET',
+    url: 'https://api.github.com/user',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: event.headers.Authorization,
+    },
+  });
+  const {
+    data: { login },
+  } = userDetails;
+  const userRecord = await Client.getSingleAttendee({
+    eventId,
+    username: login,
+  });
+  if (!userRecord) {
+    return callback(Error('User Not Found'), {
+      statusCode: 404,
+      body: 'User not found',
+    });
+  }
+  const { id } = userRecord;
+  await Client.removeAttendee({ id });
+  callback(null, {
+    statusCode: 200,
+  });
+}
+
+/****** UTILS ******/
+
 function _configureAirtable() {
   Airtable.configure({ apiKey: '__AIRTABLE_API_KEY__' });
-  return Airtable.base('__AIRTABLE_BASE_ID__');
+  return Airtable.base('__AIRTABLE_BASE_ID__')('Attendees');
+}
+
+function AirtableService(client) {
+  return {
+    async getSingleAttendee({ eventId, username }) {
+      let attendees = [];
+      await client
+        .select({
+          filterByFormula: `AND(
+            SEARCH("${eventId}",{Event ID}),
+            SEARCH("${username}",{Github Username})
+          )`,
+        })
+        .eachPage((records, fetchNextPage) => {
+          records.forEach(function(record) {
+            attendees.push(record.fields);
+          });
+          fetchNextPage();
+        });
+      return attendees[0]; // there should only be one
+    },
+    async listAttendees({ eventId }) {
+      let attendees = [];
+      await client
+        .select({
+          filterByFormula: `SEARCH("${eventId}",{Event ID})`,
+        })
+        .eachPage((records, fetchNextPage) => {
+          records.forEach(function(record) {
+            attendees.push(record.fields);
+          });
+          fetchNextPage();
+        });
+      return attendees;
+    },
+    insertAttendee({ name, login, eventId }) {
+      return client.create([
+        {
+          fields: {
+            Name: name,
+            'Github Username': login,
+            'Event ID': eventId,
+            Type: 'Attendee',
+            'Created Date': new Date().toISOString(),
+          },
+        },
+      ]);
+    },
+    removeAttendee({ id }) {
+      return client.destroy([id]);
+    },
+  };
 }
