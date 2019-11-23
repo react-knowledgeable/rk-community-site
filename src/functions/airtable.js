278a9f398212edf293b2d5fcbb20569466b0fe82
Thomas Chia <thomas.h.chia@googlemail.com>
2019-11-23 18:36:36 +0800

    bump

--- src/functions/airtable.js
+++ src/functions/airtable.js
@@ -1,60 +1,60 @@
-import Airtable from 'airtable'
+import Airtable from 'airtable';
 
 const ERROR_MSGS = {
   UNSUPPORTED_METHOD: 'Unsupported method',
   UNKNOWN_ERROR: 'Server Error',
-}
+};
 
 export const handler = async (event, _, callback) => {
   try {
-    const atClient = _configureAirtable()
+    const atClient = _configureAirtable();
     switch (event.httpMethod) {
       case 'POST':
-        await insertAttendee(atClient, event, callback)
-        break
+        await insertAttendee(atClient, event, callback);
+        break;
       case 'GET':
-        await retrieveAttendees(atClient, event, callback)
-        break
+        await retrieveAttendees(atClient, event, callback);
+        break;
       default:
         callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
           status: 405,
           body: ERROR_MSGS.UNSUPPORTED_METHOD,
-        })
+        });
     }
   } catch (e) {
     callback(Error(e), {
       status: 500,
       body: ERROR_MSGS.UNKNOWN_ERROR,
-    })
+    });
   }
-}
+};
 
 async function retrieveAttendees(Client, event, callback) {
-  let attendees = []
-  let selectOpts = {}
-  let filterConfig = []
-  const { eventId, username } = event.queryStringParameters
+  let attendees = [];
+  let selectOpts = {};
+  let filterConfig = [];
+  const { eventId, username } = event.queryStringParameters;
   if (eventId) {
-    filterConfig.push(`SEARCH("${eventId}",{Event ID})`)
+    filterConfig.push(`SEARCH("${eventId}",{Event ID})`);
   }
   if (username) {
-    filterConfig.push(`SEARCH("${username}",{Github Username})`)
+    filterConfig.push(`SEARCH("${username}",{Github Username})`);
   }
   if (filterConfig.length > 0) {
-    selectOpts = `AND(${filterConfig.join(',')})`
+    selectOpts = `AND(${filterConfig.join(',')})`;
   }
   await Client('Attendees')
     .select(selectOpts)
     .eachPage((records, fetchNextPage) => {
       records.forEach(function(record) {
-        attendees.push(record.fields)
-      })
-      fetchNextPage()
-    })
+        attendees.push(record.fields);
+      });
+      fetchNextPage();
+    });
   callback(null, {
     status: 200,
     body: JSON.stringify(attendees),
-  })
+  });
 }
 
 async function insertAttendee(Client, event, callback) {
@@ -62,9 +62,9 @@ async function insertAttendee(Client, event, callback) {
     return callback(Error({ message: ERROR_MSGS.UNSUPPORTED_METHOD }), {
       status: 405,
       body: ERROR_MSGS.UNSUPPORTED_METHOD,
-    })
+    });
   }
-  const { eventId, name, username } = JSON.parse(event.body)
+  const { eventId, name, username } = JSON.parse(event.body);
   await Client('Attendees').create([
     {
       fields: {
@@ -75,14 +75,14 @@ async function insertAttendee(Client, event, callback) {
         'Created Date': new Date().toISOString(),
       },
     },
-  ])
+  ]);
   callback(null, {
     status: 200,
     body: JSON.stringify({ name, eventId }),
-  })
+  });
 }
 
 function _configureAirtable() {
-  Airtable.configure({ apiKey: '__AIRTABLE_API_KEY__' })
-  return Airtable.base('__AIRTABLE_BASE_ID__')
+  Airtable.configure({ apiKey: '__AIRTABLE_API_KEY__' });
+  return Airtable.base('__AIRTABLE_BASE_ID__');
 }
