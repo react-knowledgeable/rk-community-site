6ae0f6a8c895ef573444538ab875e0198ff15413
Wei Gao <wgao19@gmail.com>
2019-12-01 00:17:06 +0800

    display tooltip on avatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './s.module.scss';
 
 export default ({ avatarUrl, login, url }) => (
-  <a href={url} className={s.avatarLink}>
+  <a href={url} className={s.avatarLink} title={login}>
     <img className={s.avatar} src={avatarUrl} alt={`${login}-avatar`} />
   </a>
 );
