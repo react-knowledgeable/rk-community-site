f369e7f2052205ca8f6eebbee1540b71d356de9d
Wei Gao <wgao19@gmail.com>
2019-12-01 00:20:24 +0800

    Revert "Feature/GitHub auth"

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  printWidth: 80,
-  parser: 'babel-flow',
-  semi: true,
-}
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    printWidth: 80,
+    parser: 'babel-flow',
+  }
+  
